38116143cb65562d0f40b49021f57573b84a136e
Zexa <zexa@cock.li>
2021-12-02 07:26:41 +0200

    Solve _02_1

--- _02_1/src/main.rs
+++ _02_1/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    helper::hello_world();
+    let filename = "input";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut horizontal = 0;
+    let mut depth = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let (command, amount) = line.split_at(line.find(" ").unwrap());
+        let amount = amount.trim().parse::<i32>().unwrap();
+        match command {
+            "forward" => {
+                horizontal = horizontal + amount;
+            },
+            "down" => {
+                depth = depth + amount;
+            },
+            "up" => {
+                depth = depth - amount;
+            }
+            _ => {
+                panic!("Unknown command {}", command);
+            }
+        }
+    }
+
+    let result = horizontal * depth;
+    println!("result: {}", result);
 }
